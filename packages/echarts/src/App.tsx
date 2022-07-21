99ead923dc620b23039f7c160d0f1c90e5e1e1a9
ericlee33 <ericfly33@gmail.com>
2022-07-21 19:24:58 +0800

    update

--- packages/echarts/src/App.tsx
+++ packages/echarts/src/App.tsx
@@ -11,28 +11,62 @@ function App() {
       var option = {
         dataset: {
           source: [
-            ['score', 'amount', 'product'],
-            [89.3, 58212, 'Matcha Latte'],
-            [57.1, 78254, 'Milk Tea'],
-            [74.4, 41032, 'Cheese Cocoa'],
-            [50.1, 12755, 'Cheese Brownie'],
-            [89.7, 20145, 'Matcha Cocoa'],
-            [68.1, 79146, 'Tea'],
-            [19.6, 91852, 'Orange Juice'],
-            [10.6, 101852, 'Lemon Juice'],
-            [32.7, 20112, 'Walnut Brownie'],
+            ['p', '124', '299'],
+            ['o', '30', '400'],
           ],
+          sourceHeader: false,
+          dimensions: ['title', 'begin', 'last'],
         },
-        xAxis: {},
-        yAxis: { type: 'category' },
+        grid: { containLabel: true, left: 60, right: 60, top: 0, bottom: 0 },
+        tooltip: {
+          show: true,
+          confine: false,
+          appendToBody: true,
+          enterable: true,
+          trigger: 'axis',
+        },
+        xAxis: { type: 'value' },
+        yAxis: {
+          type: 'category',
+          data: ['o', 'p'],
+          axisTick: { show: false },
+          axisLine: { show: false },
+        },
         series: [
           {
-            type: 'bar',
-            encode: {
-              // 将 "amount" 列映射到 X 轴。
-              x: 'amount',
-              // 将 "product" 列映射到 Y 轴。
-              y: 'product',
+            type: 'custom',
+            encode: { y: 0, x: [1, 2] },
+            itemStyle: {},
+            label: { position: 'right', show: true, color: '#666' },
+            labelLayout: { x: 1246, moveOverlap: 'shiftY' },
+            renderItem: function (params, api) {
+              var categoryIndex = api.value(0);
+              var start = api.coord([api.value(1), categoryIndex]);
+              var end = api.coord([api.value(2), categoryIndex]);
+              var height = api.size([0, 1])[1] * 0.6;
+
+              var rectShape = echarts.graphic.clipRectByRect(
+                {
+                  x: start[0],
+                  y: start[1] - height / 2,
+                  width: end[0] - start[0],
+                  height: height,
+                },
+                {
+                  x: params.coordSys.x,
+                  y: params.coordSys.y,
+                  width: params.coordSys.width,
+                  height: params.coordSys.height,
+                }
+              );
+
+              return (
+                rectShape && {
+                  type: 'rect',
+                  shape: rectShape,
+                  style: api.style(),
+                }
+              );
             },
           },
         ],
