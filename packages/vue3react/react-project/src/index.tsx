7c9eef190635745f473f18b89ca1bfc98e2e5e39
Eric Lee <ericfly33@gmail.com>
2023-04-07 21:17:44 +0800

    init project

--- packages/vue3react/react-project/src/index.tsx
+++ packages/vue3react/react-project/src/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
+import { registerMicroApps, start } from 'qiankun';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -16,4 +17,15 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
+
+const apps = [
+  {
+    name: 'vueApp', //子应用的名称
+    entry: 'http://127.0.0.1:5173', //子应用的域名
+    container: '#vue', //承载子应用的容器，在上面App.vue中定义
+    activeRule: '/vue', // 被激活的子应用的路由
+  },
+];
+registerMicroApps(apps); //注册子应用
+start(); //启动qiankun
--- packages/vue3react/vite-project/vite.config.ts
+++ packages/vue3react/vite-project/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import qiankun from 'vite-plugin-qiankun';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    // 在开发模式下需要把react()关掉
+    // https://github.com/umijs/qiankun/issues/1257
+    // react(),
+    qiankun('react-app', {
+      // 微应用名字，与主应用注册的微应用名字保持一致
+      useDevMode: true,
+    }),
+  ],
+});
