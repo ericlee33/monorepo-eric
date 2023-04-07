580da7e4b9fa53f1af664d3d87ff7eebe7f9e68b
Eric Lee <ericfly33@gmail.com>
2023-04-07 20:45:21 +0800

    update

---
+++ packages/my-node/index.js
@@ -0,0 +1,5 @@
+const abc = () => console.log(2);
+
+module.exports = {
+  abc,
+};
---
+++ packages/vue3react/react-project/src/App.tsx
@@ -0,0 +1,50 @@
+import React, { useMemo, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { useEffect } from 'react';
+
+function App() {
+  const [arr, setArr] = useState({ a: 1 });
+
+  useEffect(() => {
+    // throw new Error('123');
+  }, []);
+
+  const b = useMemo(() => {
+    return arr;
+  }, [arr]);
+
+  return (
+    <div className="App">
+      <header
+        className="App-header"
+        onClick={() =>
+          setArr((arr) => {
+            return {
+              ...arr,
+              b: 1,
+            };
+          })
+        }
+      >
+        <div className="abcd"></div>
+        {/* {b.map((t) => t)} */}
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
