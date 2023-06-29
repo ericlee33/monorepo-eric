e4c4d872a1db4a37adfc69bd3d6a118dd62bdfd8
ericlee33 <ericfly33@gmail.com>
2023-06-29 16:44:21 +0800

    update

--- packages/formily-test/src/App.tsx
+++ packages/formily-test/src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import Basic from './Cases/Basic';
 import Validate from './Cases/Validate';
-import Reaction from './Cases/Reaction';
+import VoluntaryLinkages from './Cases/VoluntaryLinkages';
+import PasstiveLinkages from './Cases/PasstiveLinkages';
 import { Card } from 'antd';
 
 const App = () => {
   return (
     <Card style={{ width: 400 }}>
       {/* <Basic /> */}
-      {/* <Reaction /> */}
-      <Validate />
+      {/* <VoluntaryLinkages /> */}
+      <PasstiveLinkages />
+      {/* <Validate /> */}
     </Card>
   );
 };
